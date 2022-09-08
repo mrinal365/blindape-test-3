d0e8f68499e53b947744dc4b7a175a236e0a8284
mrinal365 <40198111+mrinal365@users.noreply.github.com>
2022-09-09 00:15:14 +0530

    UI

---
+++ precache-manifest.1c3aac1db35a3cf8017389f8e229aebe.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f979820bedbca42d3c0d7417989b06e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d3dafaafb8da1a55740",
+    "url": "/static/css/2.0e31c611.chunk.css"
+  },
+  {
+    "revision": "e8b5beafbfb636b271b1",
+    "url": "/static/css/main.dc9449b4.chunk.css"
+  },
+  {
+    "revision": "0d3dafaafb8da1a55740",
+    "url": "/static/js/2.48ef6fbf.chunk.js"
+  },
+  {
+    "revision": "52825e981aa044651c73f1b80902057b",
+    "url": "/static/js/2.48ef6fbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8b5beafbfb636b271b1",
+    "url": "/static/js/main.7fe92ade.chunk.js"
+  },
+  {
+    "revision": "3be65accbbcb3071632c",
+    "url": "/static/js/runtime-main.91805d53.js"
+  },
+  {
+    "revision": "74496d9086d97aaeeafb3085e9957668",
+    "url": "/static/media/PressStart2P.74496d90.ttf"
+  },
+  {
+    "revision": "97d3342e7c01282731202858a548edd2",
+    "url": "/static/media/mintprice.97d3342e.png"
+  },
+  {
+    "revision": "4b18485ef558a5d41089330ef80b82e0",
+    "url": "/static/media/pic.4b18485e.gif"
+  },
+  {
+    "revision": "150985612a7e741fff4f3be8c43cdd58",
+    "url": "/static/media/smolpunks.15098561.png"
+  },
+  {
+    "revision": "d47f90f9d60aaab72e5cb378694a39c0",
+    "url": "/static/media/story.d47f90f9.png"
+  },
+  {
+    "revision": "3ab89f05c9a9bbb0152ec9e064b89be9",
+    "url": "/static/media/tel.3ab89f05.png"
+  },
+  {
+    "revision": "6ca2d940ce11d2a5111ef6ea1074698f",
+    "url": "/static/media/twi.6ca2d940.png"
+  }
+]);
